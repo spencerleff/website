8573c0e3379c0423ac85fab783e977325200fe7f
Spencer <Spencerleff@gmail.com>
2026-06-25 09:35:40 -0700

    use JS to calculate the app height instead of relying on css.  improve work cards and grids a little bit before polishing them

--- scripts.js
+++ scripts.js
@@ -7,6 +7,21 @@ function toggleNav() {
 }
 
 
+//Calculate true app height to avoid issues with IOS url bar
+function updateAppHeight() {
+    const height =
+        window.visualViewport?.height ||
+        window.innerHeight;
+
+    document.documentElement.style.setProperty(
+        '--app-height',
+        `${height}px`
+    );
+}
+
+updateAppHeight();
+
+
 //Section navigation
 const sections = [...document.querySelectorAll('.section')];
 let currentSection = 0;
@@ -213,15 +228,29 @@ window.addEventListener('scroll', () => {
 //Update view on screen resize (60fps)
 let resizeFrame = null;
 window.addEventListener('resize', () => {
+    updateAppHeight();
+
     if (isAnimating) return;
     if (resizeFrame) return;
+
     resizeFrame = requestAnimationFrame(() => {
         resizeFrame = null;
+
         const section = sections[currentSection];
         if (!section) return;
+
         window.scrollTo(0, section.offsetTop);
     });
 });
+
+
+//Update app height on browser resize
+if (window.visualViewport) {
+    window.visualViewport.addEventListener(
+        'resize',
+        updateAppHeight
+    );
+}
 
 
 //Remember the current section before rotating
